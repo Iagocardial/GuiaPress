ff4b7180449be945f1fcfe0f63c0414dd899d38c
iagocardial <iagocardial@tecsus.com.br>
2021-03-17 17:45:56 -0300

    Wip

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -54,4 +54,84 @@ router.post("/articles/delete", (req, res) => {
     }
 });
 
+router.post("/articles/update", (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Articles.update(
+        {
+            title: title,
+            body: body,
+            categoryId: category,
+            slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+            res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    })
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    let id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/articles")
+    }
+
+    Articles.findByPk(id).then(article => {
+        if(article != undefined) {
+
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {article: article, categories: categories});
+            })
+        }else{
+            res.redirect("admin/articles");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/articles");
+    })
+});
+
+router.get("/articles/page/:num", (req, res) => {
+    let page = req.params.num;
+    let offset = 0;
+
+    if(!isNaN(page) || page != 1) {
+        offset =( parseInt(page) - 1) * 4;
+    }
+
+    Articles.findAndCountAll({
+        limit: 2,
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then( articles => {
+
+        let next = true;
+        
+        if(offset + 4 >= articles.count){
+            next = false;
+        }
+
+        let result = {
+            page: parseInt(page),
+            articles: articles,
+            next: next
+        }
+        
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page", {result: result, categories: categories});
+        });
+
+        
+    })
+})
+
+
 module.exports = router;
